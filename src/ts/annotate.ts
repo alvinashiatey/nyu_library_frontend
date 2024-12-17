91b3a9e315e71f3cae87adf919d109895d6d7a01
Alvin Ashiatey <alvinashiatey@gmail.com>
2024-12-17 15:08:19 -0500

    Add PDF viewer component and integrate PDF.js for rendering

---
+++ src/ts/annotate.ts
@@ -0,0 +1,52 @@
+import * as pdfjsLib from "pdfjs-dist";
+
+async function annotatePdf(
+  pdfUrl: string | undefined,
+  container: HTMLDivElement
+) {
+  if (!pdfUrl) return;
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "/node_modules/pdfjs-dist/build/pdf.worker.min.mjs";
+  pdfjsLib.getDocument(pdfUrl).promise.then((pdfDoc) => {
+    for (let i = 1; i <= pdfDoc.numPages; i++) {
+      pdfDoc.getPage(i).then((page) => {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        container.appendChild(canvas);
+
+        const viewport = page.getViewport({ scale: 1 });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        if (context) {
+          page.render({ canvasContext: context, viewport });
+        }
+      });
+    }
+  });
+}
+
+function innitAnnotate() {
+  document.getElementById("hypothesis")?.addEventListener("click", () => {
+    // Check if Hypothesis is already loaded
+    if (!document.querySelector("script#hypothesis-script")) {
+      const script = document.createElement("script");
+      script.id = "hypothesis-script";
+      script.src = "https://hypothes.is/embed.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  });
+}
+
+async function main() {
+  // const container = document.getElementById("pdf-container") as HTMLDivElement;
+  // const pdfUrl = container.dataset.url;
+  // await annotatePdf(pdfUrl, container);
+
+  innitAnnotate();
+}
+
+main();
+
+console.log("Annotating PDFs");
