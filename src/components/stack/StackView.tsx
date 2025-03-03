9d9ce057099f5406d02f9f93599e32c092bc2d17
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-03-03 12:08:36 -0500

    Refactor StackView component; update layout and styles, enhance book display with background images and improved interactivity

--- src/components/stack/StackView.tsx
+++ src/components/stack/StackView.tsx
@@ -7,9 +7,11 @@ interface StackViewProps {
   stack?: string;
 }
 
-const StackView: React.FC<StackViewProps> = ({ bks }) => {
+const StackView: React.FC<StackViewProps> = ({ bks, stack }) => {
   const [books, setBooks] = useState<Book[]>([]);
   const [loading, setLoading] = useState(true);
+  const paddedStack = Number(stack) < 10 ? `0${stack}` : stack;
+  const background = `url(/public/range/r${paddedStack}.png)`;
 
   useEffect(() => {
     if (bks.length) {
@@ -30,21 +32,42 @@ const StackView: React.FC<StackViewProps> = ({ bks }) => {
         <div>Loading...</div>
       ) : (
         <div className={styles.container}>
-          <div className={styles.preview}></div>
-          <div className={styles.books}>
-            {books.map((book) => (
-              <div key={book.title} className={styles.book}>
-                {book.files && (
-                  <div className={styles.image}>
-                    <img src={book.files[0].url} alt={book.title} />
-                  </div>
-                )}
-                <div className={styles.info}>
-                  <h2>{book.title}</h2>
-                  <p>{truncateText(book.description)}</p>
-                </div>
+          <div className={styles.preview}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: background }}
+            />
+          </div>
+          <div className={styles.content}>
+            <header>
+              <div className={styles.range}>
+                <h2>
+                  Range <span className="text-white-50">{paddedStack}</span>
+                </h2>
               </div>
-            ))}
+              <div className="back">
+                <a href="/" className="btn btn-black">
+                  x
+                </a>
+              </div>
+            </header>
+            <div className={styles.books}>
+              {books.map((book) => (
+                <a href={`/book/${book.slug}`}>
+                  <div key={book.title} className={styles.book}>
+                    {book.files && (
+                      <div className={styles.image}>
+                        <img src={book.files[0].url} alt={book.title} />
+                      </div>
+                    )}
+                    <div className={styles.info}>
+                      <h2>{book.title}</h2>
+                      <p>{truncateText(book.description)}</p>
+                    </div>
+                  </div>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       )}
